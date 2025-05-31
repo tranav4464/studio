90b3b0e8b8d74bb2a9dce20e6e42f3e41489e1b7
Tranav Tyagi <tranav50@gmail.com>
2025-05-31 04:26:10 +0000

    you changed the outline variant button color in dark mode also which i didn't wanted

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -13,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-[#02C39A] bg-background text-[#02C39A] hover:bg-[#02C39A] hover:text-primary-foreground",
+          "border border-[#02C39A] bg-background text-[#02C39A] hover:bg-[#02C39A] hover:text-primary-foreground dark:border-input dark:text-foreground dark:hover:bg-accent dark:hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
